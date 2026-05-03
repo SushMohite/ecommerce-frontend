ce8f5bf18bfce957bcd603af07b8ca722556f90f
SushMohite <sushantmohite85030@gmail.com>
2026-05-03 16:41:46 +0530

    updated hero section

--- scripts/app.js
+++ scripts/app.js
@@ -1,1 +1,12 @@
-console.log("E-Commerce Website Loaded");
+const hamburger = document.getElementById("hamburger");
+const navMenu = document.getElementById("nav-menu");
+
+hamburger.addEventListener("click", () => {
+    navMenu.classList.toggle("active");
+});
+document.querySelector(".cta-btn").addEventListener("click", (e) => {
+    e.preventDefault();
+    document.querySelector("#products").scrollIntoView({
+        behavior: "smooth"
+    });
+});
